6c64e2276e7335fd7ca181881bc290b60cd6a013
kkrahman <kkrahman1992@gmail.com>
2021-12-19 10:24:31 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const app = express()
 const port = 3000
+// get api
 app.get('/', function (req, res) {
   res.send('Hello World123')
 })
+app.get('/login/', function (req, res) {
+  res.send('Welcome to login page')
+})
+app.get('/admin/', function (req, res) {
+  res.send('Welcome to admin page')
+})
+app.get('/', function (req, res) {
+  res.send('Hello World123')
+})
+
 // app.listen(port, displayMessage())
 // function displayMessage(){
 //   console.log("My project is running . . . . .")
